bd3395b78a91e9281f4fa6862cd531b60549b4a8
muzhichan <kantchenjn@163.com>
2021-04-19 10:25:43 +0800

    init commit

---
+++ zsh.sh
@@ -0,0 +1,12 @@
+
+set -e
+
+# echo $SHELL
+# cat /etc/shells
+
+apt install -y curl wget
+apt install -y zsh
+chsh -s /bin/zsh
+# theme: bira
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
