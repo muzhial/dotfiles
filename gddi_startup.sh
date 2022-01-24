a36b086d69ef5269ec3eb606ca90cd0b3b078860
muzhichan <kantchenjn@163.com>
2022-01-24 16:32:59 +0800

    tmux style

--- gddi_startup.sh
+++ gddi_startup.sh
@@ -13,6 +13,10 @@ ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
 ## config .tmux.cong.local set mouse mode
 sed -i 's/#set -g mouse on/set -g mouse on/' .tmux.conf.local
+sed -i 's/tmux_conf_theme_status_left=" ❐ #S | ↑#{?uptime_y, #{uptime_y}y,}#{?uptime_d, #{uptime_d}d,}#{?uptime_h, #{uptime_h}h,}#{?uptime_m, #{uptime_m}m,} "/tmux_conf_theme_status_left=" #S "/' .tmux.conf.local
+sed -i 's/tmux_conf_theme_status_right=" #{prefix}#{pairing}#{synchronized}#{?battery_status,#{battery_status},}#{?battery_bar, #{battery_bar},}#{?battery_percentage, #{battery_percentage},} , %R , %d %b | #{username}#{root} | #{hostname} "/tmux_conf_theme_status_right=" #{prefix}#{pairing}#{synchronized} | #{username}#{root} | #{hostname} "/' .tmux.conf.local
+sed -i 's/tmux_conf_theme_status_right_fg="$tmux_conf_theme_colour_12,$tmux_conf_theme_colour_12,$tmux_conf_theme_colour_12"/tmux_conf_theme_status_right_fg="$tmux_conf_theme_colour_12,$tmux_conf_theme_colour_12,$tmux_conf_theme_colour_12"/' .tmux.conf.local
+sed -i 's/tmux_conf_theme_status_right_bg="$tmux_conf_theme_colour_15,$tmux_conf_theme_colour_15,$tmux_conf_theme_colour_15"/tmux_conf_theme_status_right_bg="$tmux_conf_theme_colour_15,$tmux_conf_theme_colour_15,$tmux_conf_theme_colour_15"/' .tmux.conf.local
 
 
 echo "===> install python modules"
--- muzhi.zsh-theme
+++ muzhi.zsh-theme
@@ -12,7 +12,7 @@ PROMPT='
 %{$fg[magenta]%}%n%{$reset_color%}@%{$fg[yellow]%}%m%{$reset_color%}:)%{$fg_bold[green]%}%~%{$reset_color%}$(git_prompt_info)
 $(virtualenv_info)$(prompt_char) '
 
-ZSH_THEME_GIT_PROMPT_PREFIX=" [%{$fg[magenta]%}]"
+ZSH_THEME_GIT_PROMPT_PREFIX="::%{$fg[magenta]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[green]%}!"
 ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[green]%}?"
