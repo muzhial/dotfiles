c7a69e44f67e9469a4d8ead497764c69a4c565ea
muzhichan <kantchenjn@163.com>
2022-03-11 10:33:04 +0800

    nullptr

---
+++ dockerfile/readme.md
@@ -0,0 +1,3 @@
+## ref
+
+- [https://github.com/deluan/zsh-in-docker](https://github.com/deluan/zsh-in-docker)
--- zsh.sh
+++ zsh.sh
@@ -1,5 +1,5 @@
 ## zsh
-if [ $1 -eq 1 ]; then
+# if [ $1 -eq 1 ]; then
 if command -v zsh > /dev/null 2>&1; then
     echo -e "\n===> exists zsh"
     echo "===> install oh-my-zsh"
@@ -10,15 +10,12 @@ else
     sudo chsh -s /bin/zsh
     # theme: bira | murilasso | rgm
     echo "===> install oh-my-zsh"
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
 fi
-fi
+# fi
 
-if [ $1 -eq 1 ]; then
-    cp ~/.dotfiles/muzhi.zsh-theme ~/.oh-my-zsh/themes/
-    sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="muzhi"/' ~/.zshrc
+cp ~/.dotfiles/muzhi.zsh-theme ~/.oh-my-zsh/themes/
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="muzhi"/' ~/.zshrc
 
-    echo "PATH=/opt/conda/bin:$PATH" >> ~/.zshrc
-    # source ~/.zshrc
-fi
-
+echo "PATH=/opt/conda/bin:$PATH" >> ~/.zshrc
+# source ~/.zshrc
