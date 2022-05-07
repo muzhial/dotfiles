21e4548544f4bd35d8c030c4aae5fc7d50dc1df3
muzhichan <kantchenjn@163.com>
2022-05-07 14:51:27 +0800

    nullptr

--- gddi_startup.sh
+++ gddi_startup.sh
@@ -2,7 +2,7 @@ set -e
 
 echo "PATH=/opt/conda/bin:$PATH" >> ~/.bashrc
 
-sudo apt-key del 7fa2af80
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.0-1_all.deb
-sudo dpkg -i cuda-keyring_1.0-1_all.deb && rm cuda-keyring_1.0-1_all.deb
+# sudo apt-key del 7fa2af80
+# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.0-1_all.deb
+# sudo dpkg -i cuda-keyring_1.0-1_all.deb && rm cuda-keyring_1.0-1_all.deb
 sudo rm /etc/apt/sources.list.d/cuda.list
